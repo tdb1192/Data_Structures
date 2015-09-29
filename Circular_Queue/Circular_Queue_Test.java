4ff1504d8db84f1c791c0a78bfb0d273c426cc5f
tdb1192 <tdb1192@yahoo.com>
2015-09-29 15:28:06 -0400

    Quick Annotations to CQ for HW

--- Circular_Queue/Circular_Queue_Test.java
+++ Circular_Queue/Circular_Queue_Test.java
@@ -3,8 +3,8 @@ import java.io.InputStreamReader;
 
 public class Circular_Queue_Test {	
 	public static void main( String[] args ) {
+		
 		Circular_Queue q = new Circular_Queue();
-
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String response;
 
@@ -12,6 +12,7 @@ public class Circular_Queue_Test {
 			
 			do {
 				
+				// Bases which operation to do on user input
 				System.out.print("Please select an option: ");
 				response = reader.readLine();
 				switch(response) {
@@ -23,11 +24,17 @@ public class Circular_Queue_Test {
 							break;
 					case "l":	q.printLogical();
 							break;
+					case "q":
+							break;
+							
+					// Gives help message when user enters invalid option
 					default:	System.out.println("Options are i/d/p/l/q");
 								System.out.println();
 							break;	
 				}
 			}
+			
+			// Quits when user enters "q"
 			while(!response.equals("q"));
 
 		}
