0097412ecdd45facd3a879c6892aef6853465edb
Tyler Brooks <tdb1192@yahoo.com>
2015-11-10 10:21:12 -0500

    Binary Search Script Added

--- Binary_Search/BinarySearch.java
+++ Binary_Search/BinarySearch.java
@@ -1,6 +1,5 @@
 public class BinarySearch {
-	
-<<<<<<< HEAD
+
 	public static final int NOT_FOUND = -1;
 
 	//Perform a binary search that makes two comparisons per level
@@ -48,30 +47,3 @@ public class BinarySearch {
 		return NOT_FOUND;
 	}
 }
-=======
-	public static <AnyType extends Comparable<? super AnyType>> int binarySearch( AnyType[] a, AnyType x) {
-		
-		final int NOT_FOUND = -1;
-		int low = 0;
-		int high = a.length - 1;
-		int mid;
-
-		while (low <= high) {
-			
-			mid = (low + high) / 2;
-			
-			if (a[mid].compareTo(x) < 0) {
-				low = mid + 1;
-			}
-			else if(a[mid].compareTo(x) > 0) {
-				high = mid - 1;
-			}
-			else {
-				return mid;
-			}
-		}
-		
-		return NOT_FOUND;
-	}
-}
->>>>>>> origin/master
